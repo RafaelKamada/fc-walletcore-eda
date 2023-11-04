8ccc573ffafe5351501012127fea4a7f7a9684bc
RafaelKamada <rafael-kamada2007@hotmail.com>
2023-11-04 16:36:22 -0300

    Redefinindo testes

--- internal/usecase/create_transaction/create_transaction_test.go
+++ internal/usecase/create_transaction/create_transaction_test.go
@@ -1,10 +1,12 @@
 package create_transaction
 
 import (
+	"context"
 	"testing"
 
 	"github.com/RafaelKamada/fc-ms-wallet/internal/entity"
 	"github.com/RafaelKamada/fc-ms-wallet/internal/event"
+	"github.com/RafaelKamada/fc-ms-wallet/internal/usecase/mocks"
 	"github.com/RafaelKamada/fc-ms-wallet/pkg/events"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -42,12 +44,15 @@ func TestCreateTransactionUseCase_Execute(t *testing.T) {
 	account2 := entity.NewAccount(client2)
 	account2.Credit(1000)
 
-	mockAccount := &AccountGatewayMock{}
-	mockAccount.On("FindByID", account1.ID).Return(account1, nil)
-	mockAccount.On("FindByID", account2.ID).Return(account2, nil)
+	// mockAccount := &AccountGatewayMock{}
+	// mockAccount.On("FindByID", account1.ID).Return(account1, nil)
+	// mockAccount.On("FindByID", account2.ID).Return(account2, nil)
 
-	mockTransaction := &TransactionGatewayMock{}
-	mockTransaction.On("Create", mock.Anything).Return(nil)
+	// mockTransaction := &TransactionGatewayMock{}
+	// mockTransaction.On("Create", mock.Anything).Return(nil)
+
+	mockUow := &mocks.UowMock{}
+	mockUow.On("Do", mock.Anything, mock.Anything).Return(nil)
 
 	inputDto := CreateTransactionInputDTO{
 		AccountIDFrom: account1.ID,
@@ -57,13 +62,16 @@ func TestCreateTransactionUseCase_Execute(t *testing.T) {
 
 	dispatcher := events.NewEventDispatcher()
 	event := event.NewTransactionCreated()
+	ctx := context.Background()
 
-	uc := NewCreateTransactionUseCase(mockTransaction, mockAccount, dispatcher, event)
-	output, err := uc.Execute(inputDto)
+	uc := NewCreateTransactionUseCase(mockUow, dispatcher, event)
+	output, err := uc.Execute(ctx, inputDto)
 	assert.Nil(t, err)
 	assert.NotNil(t, output)
-	mockAccount.AssertExpectations(t)
-	mockTransaction.AssertExpectations(t)
-	mockAccount.AssertNumberOfCalls(t, "FindByID", 2)
-	mockTransaction.AssertNumberOfCalls(t, "Create", 1)
+	// mockAccount.AssertExpectations(t)
+	// mockTransaction.AssertExpectations(t)
+	// mockAccount.AssertNumberOfCalls(t, "FindByID", 2)
+	// mockTransaction.AssertNumberOfCalls(t, "Create", 1)
+	mockUow.AssertExpectations(t)
+	mockUow.AssertNumberOfCalls(t, "Do", 1)
 }
